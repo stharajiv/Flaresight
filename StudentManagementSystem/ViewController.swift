77ac425e7381fcea480923fcad94be5366e9c42c
Rajiv Shrestha <rb@darklegeds-mac.local>
2018-10-30 13:18:33 +0545

    Initial Commit

---
+++ StudentManagementSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentManagementSystem
+//
+//  Created by Rajiv Shrestha on 10/30/18.
+//  Copyright © 2018 Flaresight Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
